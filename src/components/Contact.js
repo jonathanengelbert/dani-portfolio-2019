2cbc2a1d2cff77c76cc8cf6ab085d52e75095fc1
Jon Engelbert <jonathanengelbert@gmail.com>
2019-11-10 15:09:49 -0500

    Adds Contact page/component with social media icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
-const about = props => {
-    return(
-      <div id="about" className="section">
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+const Contact = props => {
+  return (
+    <div id="contact">
+      <div>
+        <a
+          target={"_blank"}
+          href="https://www.instagram.com/sweaterpenguin/?hl=en"
+          title={"Instagram"}
+        >
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
       </div>
-    );
-}
+      <div>
+        <a target={"_blank"} href={"mailto:dtatarian@live.com"} title={"Email"}>
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
+      </div>
+      <div>
+        <a
+          target={"_blank"}
+          href="https://www.linkedin.com/in/danielle-tatarian-4742a1133/"
+          title={"LinkedIn"}
+        >
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default about;
+export default Contact;
